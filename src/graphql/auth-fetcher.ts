fc9ce9096e450842aa117791d252e17b6234adaa
Ethen-007 <ethenbiju@gmail.com>
2024-04-03 00:00:42 +0530

    changed fetcher func

--- src/graphql/auth-fetcher.ts
+++ src/graphql/auth-fetcher.ts
@@ -1,21 +1,32 @@
 // logic that we want to run every time we send a request
 //to the lens graphql server
-export default function fetcher<TData, TVariables>(endpoint: string, requestInit: RequestInit, query: string, variables?: TVariables) {
-    return async (): Promise<TData> => {
-        const res = await fetch(endpoint, {
-            method: 'POST',
-            ...requestInit,
-            body: JSON.stringify({query, variables}),
-        });
+// lens graphql server endpoint
+export const fetcher= <TData, TVariables>(
+  query: string,
+  variables?: TVariables,
+  options?: RequestInit['headers']
+): (() => Promise<TData>) => {
+  return async () => {
+    const res = await fetch("./schema.graphql/", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...options
+          //Auth header here.
+      },
+      body: JSON.stringify({
+        query,
+        variables
+      })
+    })
 
-        const json = await res.json();
+    const json = await res.json()
 
-        if (json.errors) {
-            const {message} = json.errors[0];
-
-            throw new Error(message);
-        }
-
-        return json.data;
+    if (json.errors) {
+      const { message } = json.errors[0] || {}
+      throw new Error(message || 'Error…')
     }
+
+    return json.data
+  }
 }
